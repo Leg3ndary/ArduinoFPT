06a039022eff5c7d849aeaea68cc3f127a057ef2
_Leg3ndary <ben.zhou.2006@gmail.com>
2023-05-29 19:05:02 -0400

    Altering code

--- src/main.ino
+++ src/main.ino
@@ -1,29 +1,54 @@
-// Pin connected to Pin 12 of 74HC595 (Latch)
-int latchPin = 8;
-// Pin connected to Pin 11 of 74HC595 (Clock)
-int clockPin = 12;
-// Pin connected to Pin 14 of 74HC595 (Data)
-int dataPin = 11;
+/*
+This is code for a friend and I's arduino fpt, without the diagram it's kinda useless, so just don't copy ig!
 
-const int bits[8] = {1, 2, 4, 8, 16, 32, 64, 128};
+Total Memory Available: 2048 bytes
+*/
 
+/*
+Datas is pins connected to Pin 14 of 74HC595 (BLUE)
+Latches is pins connected to Pin 12 of 74HC595 (GREEN)
+Clocks is pins connected to Pin 11 of 74HC595 (YELLOW)
+
+Total Memory used here = 3 * 3 * 2 = 18 bytes
+*/
+const int datas[3] = {13, 10, 7};
+const int latches[3] = {12, 9, 6};
+const int clocks[3] = {11, 8, 5};
+
+/* State is what state the game is in.
+0: Home Screen
+- LEDs will be flashing randomly maybe we add patterns
+- Piezos will be playing some random music
+
+1: In game
+- LEDs will be moving around in until it hits the goal
+
+2: Game over
+- Has to push reset to be brought to home screen
+
+Memory used: 2 bytes
+*/
+int state = 0;
+
+// Setting up the pins, all the pins for the bit shifters are outputs.
 void setup() {
-  // Set pins to output
-  pinMode(latchPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
+  for (int i = 0; i < 3; i++) {
+    pinMode(datas[i], OUTPUT);
+    pinMode(latches[i], OUTPUT);
+    pinMode(clocks[i], OUTPUT);
+  }
 }
 
-
 void loop() {
-  for (int i = 0; i < 8; i++) {
-    // Set latchPin low to allow data flow
-    digitalWrite(latchPin, LOW);
-    shiftOut(bits[i]);
-    // Set latchPin to high to lock and send data
-    digitalWrite(latchPin, HIGH);
-    delay(100);
-  }
+  // for (int i = 0; i < 8; i++) {
+  //   // Set latchPin low to allow data flow
+  //   digitalWrite(latchPin, LOW); 
+  //   shiftOut(bits[i]);
+  //   // Set latchPin to high to lock and send data
+  //   digitalWrite(latchPin, HIGH);
+  //   delay(100);
+  // }
+  if (state == 0)
 }
 
 
