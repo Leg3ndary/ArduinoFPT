818bcb7f8beb40e7f5828d704cb371afb94bc868
Ben <57199957+Leg3ndary@users.noreply.github.com>
2023-06-02 13:25:21 -0400

    Push changes

--- src/main.ino
+++ src/main.ino
@@ -5,72 +5,60 @@ Total Memory Available: 2048 bytes
 */
 
 /*
-Datas is pins connected to Pin 14 of 74HC595 (BLUE)
-Latches is pins connected to Pin 12 of 74HC595 (GREEN)
-Clocks is pins connected to Pin 11 of 74HC595 (YELLOW)
-
-start is the start button. (Start)
-interact is the interaction button. (X)
-
-Total Memory used here = 4 * 3 * 2 = 24 bytes
+The following are all constants, first group is pins, seconds is just other data that won't be changed.
 */
-const int datas[4] = {13, 10, 7, 4};
-const int latches[4] = {12, 9, 6, 3};
-const int clocks[4] = {11, 8, 5, 2};
+const int dataPins[4] = {13, 10, 7, 4};
+const int latchPins[4] = {12, 9, 6, 3};
+const int clockPins[4] = {11, 8, 5, 2};
+const int startButton = 14;
+const int interactButton = 15;
+const int speakerPin = 19;
 
-const int start = 14;
-const int interact = 15;
-const int speaker1 = 18;
-const int speaker2 = 19;
 const int bits[8] = {128, 64, 32, 16, 8, 4, 2, 1};
+const int speeds[9] = {660, 580, 480, 360, 320, 300, 280, 260, 245};
 
-/* State is what state the game is in.
-Refresh is how many milliseconds between each refresh of checking the Inputs
-Speed is how fast it takes to get to the next LED
+/*
+The following are all things that might change throughout the program
 */
 int state = 1;
-int currentLed = 0;
 bool clockwise = true;
 
-int refresh = 5;
-const int speeds[9] = {660, 580, 480, 360, 320, 300, 280, 260, 245};
+int currentLed = 0;
 int currentSpeed = speeds[0];
 int currentTime = 0;
 
 int startState = 0;
+int lastStartState = 0;
 int interactState = 0;
 int lastInteractState = 0;
 
 int gameRunLR = 0;
 
 /*
-Setting up all the pins.
-Output pins are all the bitShifter pins, as well as the speakers.
-INPUT_PULLUP uses the inbuilt resistors to allow us not to use resistors for start and interact.
+Setting up all the pins to the correct mode.
 */
 void setup() {
   Serial.begin(9600);
   for (int i = 0; i < 4; i++) {
-    pinMode(datas[i], OUTPUT);
-    pinMode(latches[i], OUTPUT);
-    pinMode(clocks[i], OUTPUT);
+    pinMode(dataPins[i], OUTPUT);
+    pinMode(latchPins[i], OUTPUT);
+    pinMode(clockPins[i], OUTPUT);
   }
-  pinMode(start, INPUT);
-  pinMode(interact, INPUT);
-  pinMode(speaker1, OUTPUT);
-  pinMode(speaker2, OUTPUT);
+  pinMode(startButton, INPUT);
+  pinMode(interactButton, INPUT);
+  pinMode(speakerPin, OUTPUT);
 }
 
 void shiftOut(byte dataOut, int bitShifter) {
   // Shift out 8 bits LSB first, on rising edge of clock
   boolean pinState;
   // Clear shift register ready for sending data
-  digitalWrite(datas[bitShifter], LOW);
-  digitalWrite(clocks[bitShifter], LOW);
+  digitalWrite(dataPins[bitShifter], LOW);
+  digitalWrite(clockPins[bitShifter], LOW);
   // For each bit in dataOut send out a bit
   for (int i = 0; i <= 7; i++) {
     // Set clockPin to LOW prior to sending bit
-    digitalWrite(clocks[bitShifter], LOW);
+    digitalWrite(clockPins[bitShifter], LOW);
     // If the value of DataOut and (logical AND) a bitmask are true, set pinState to 1 (HIGH)
     if (dataOut & (1 << i)) {
       pinState = HIGH;
@@ -78,19 +66,19 @@ void shiftOut(byte dataOut, int bitShifter) {
       pinState = LOW;
     }
     // Sets dataPin to HIGH or LOW depending on pinState
-    digitalWrite(datas[bitShifter], pinState);
+    digitalWrite(dataPins[bitShifter], pinState);
     // Send bit out on rising edge of clock
-    digitalWrite(clocks[bitShifter], HIGH);
+    digitalWrite(clockPins[bitShifter], HIGH);
   }
   // Stop shifting out data
-  digitalWrite(clocks[bitShifter], LOW);
+  digitalWrite(clockPins[bitShifter], LOW);
 }
 
 void clearLeds() {
   for (int i = 0; i < 4; i++) {
-    digitalWrite(latches[i], LOW);
+    digitalWrite(latchPins[i], LOW);
     shiftOut(1, i);
-    digitalWrite(latches[i], HIGH);
+    digitalWrite(latchPins[i], HIGH);
   }
 }
 
@@ -104,10 +92,10 @@ void gameRun() {
     int bit = currentLed % 7;
     clearLeds();
     // Set latchPin low to allow data flow
-    digitalWrite(latches[bitShifter], LOW); 
+    digitalWrite(latchPins[bitShifter], LOW); 
     shiftOut(bits[bit], bitShifter);
     // Set latchPin to high to lock and send data
-    digitalWrite(latches[bitShifter], HIGH);
+    digitalWrite(latchPins[bitShifter], HIGH);
 
     if (clockwise) {
       currentLed++;
@@ -131,15 +119,14 @@ void gameOver() {
 
 void loop() {
   currentTime = millis();
-  startState = digitalRead(start);
-  interactState = digitalRead(interact);
+  startState = digitalRead(startButton);
+  interactState = digitalRead(interactButton);
 
   if (state == 0) {
     gameStart();
   } else if (state == 1)
   {
     if (!lastInteractState && interactState) {
-      Serial.println(!clockwise);
       clockwise = !clockwise;
     }
     gameRun();
@@ -149,5 +136,6 @@ void loop() {
     state = 0;
   }
 
+  lastStartState = startState;
   lastInteractState = interactState;
 }
