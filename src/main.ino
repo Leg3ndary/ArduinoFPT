a61da1d901b404082b9341aef8ce9ba9927229eb
Ben <ben.zhou.2006@gmail.com>
2023-06-08 11:46:37 -0400

    Music finished

--- src/main.ino
+++ src/main.ino
@@ -2,28 +2,54 @@
 Pop the lock gamebox code, Copyright Leg3ndary 2023.
 */
 
+#define D3  147
+#define E3  165
+#define FS3 185
+#define G3  196
+#define A3  220
+#define AS3 233
+#define B3  247
+#define C4  262
+#define CS4 277
+#define D4  294
+#define DS4 311
+#define E4  330
+#define F4  349
+#define FS4 370
+#define G4  392
 #define GS4 415
 #define A4  440
+#define AS4 466
 #define B4  494
 #define C5  523
+#define CS5 554
 #define D5  587
+#define DS5 622
 #define E5  659
 #define F5  698
+#define FS5 740
 #define G5  784
 #define GS5 831
 #define A5  880
+#define AS5 932
+#define B5  988
+#define C6  1047
+#define CS6 1109
+#define DS6 1245
+#define F6  1397
 #define REST 0
 
 // Constants
 const int dataPins[4] = {13, 10, 7, 4};
 const int latchPins[4] = {12, 9, 6, 3};
 const int clockPins[4] = {11, 8, 5, 2};
-const int startButton = 14;
-const int interactButton = 15;
+const int startPin = 14;
+const int interactPin = 15;
 const int seedPin = 18;
 const int speakerPin = 19;
 
 const int scoreAnimationSpeed = 3000;
+
 const int speeds[7] = {150, 120, 95, 75, 60, 50, 40};
 const int difficultyLeds[7][4] = {
   {
@@ -49,8 +75,8 @@ const int difficultyLeds[7][4] = {
   }
 };
 
-// Melodies: 
-const int melody[] = {
+// Melodies
+const int tetrisMelody[] = {
   E5, 4,  B4,8,  C5,8,  D5,4,  C5,8,  B4,8,
   A4, 4,  A4,8,  C5,8,  E5,4,  D5,8,  C5,8,
   B4, -4,  C5,8,  D5,4,  E5,4,
@@ -80,15 +106,301 @@ const int melody[] = {
   C5,4,   E5,4,  A5,2,
   GS5,2,
 };
-const int musicTempo = 200;
+const int tetrisTempo = 200;
 
+const int pinkPantherMelody[] = {
+  REST,2, REST,4, REST,8, DS4,8, 
+  E4,-4, REST,8, FS4,8, G4,-4, REST,8, DS4,8,
+  E4,-8, FS4,8,  G4,-8, C5,8, B4,-8, E4,8, G4,-8, B4,8,   
+  AS4,2, A4,-16, G4,-16, E4,-16, D4,-16, 
+  E4,2, REST,4, REST,8, DS4,4,
 
-int notes = sizeof(melody) / sizeof(melody[0]) / 2; 
-int divider = 0, noteDuration = 0;
-int wholenote = (60000 * 4) / musicTempo;
-int currentNote = 0;
+  E4,-4, REST,8, FS4,8, G4,-4, REST,8, DS4,8,
+  E4,-8, FS4,8,  G4,-8, C5,8, B4,-8, G4,8, B4,-8, E5,8,
+  DS5,1,   
+  D5,2, REST,4, REST,8, DS4,8, 
+  E4,-4, REST,8, FS4,8, G4,-4, REST,8, DS4,8,
+  E4,-8, FS4,8,  G4,-8, C5,8, B4,-8, E4,8, G4,-8, B4,8,   
+  
+  AS4,2, A4,-16, G4,-16, E4,-16, D4,-16, 
+  E4,-4, REST,4,
+  REST,4, E5,-8, D5,8, B4,-8, A4,8, G4,-8, E4,-8,
+  AS4,16, A4,-8, AS4,16, A4,-8, AS4,16, A4,-8, AS4,16, A4,-8,   
+  G4,-16, E4,-16, D4,-16, E4,16, E4,16, E4,2,
+ 
+};
+const int pinkPantherTempo = 160;
+
+int neverGonneGiveYouUpMelody[] = {
+  D5,-4, E5,-4, A4,4, //1
+  E5,-4, FS5,-4, A5,16, G5,16, FS5,8,
+  D5,-4, E5,-4, A4,2,
+  A4,16, A4,16, B4,16, D5,8, D5,16,
+  D5,-4, E5,-4, A4,4, //repeat from 1
+  E5,-4, FS5,-4, A5,16, G5,16, FS5,8,
+  D5,-4, E5,-4, A4,2,
+  A4,16, A4,16, B4,16, D5,8, D5,16,
+  REST,4, B4,8, CS5,8, D5,8, D5,8, E5,8, CS5,-8,
+  B4,16, A4,2, REST,4, 
+
+  REST,8, B4,8, B4,8, CS5,8, D5,8, B4,4, A4,8, //7
+  A5,8, REST,8, A5,8, E5,-4, REST,4, 
+  B4,8, B4,8, CS5,8, D5,8, B4,8, D5,8, E5,8, REST,8,
+  REST,8, CS5,8, B4,8, A4,-4, REST,4,
+  REST,8, B4,8, B4,8, CS5,8, D5,8, B4,8, A4,4,
+  E5,8, E5,8, E5,8, FS5,8, E5,4, REST,4,
+   
+  D5,2, E5,8, FS5,8, D5,8, //13
+  E5,8, E5,8, E5,8, FS5,8, E5,4, A4,4,
+  REST,2, B4,8, CS5,8, D5,8, B4,8,
+  REST,8, E5,8, FS5,8, E5,-4, A4,16, B4,16, D5,16, B4,16,
+  FS5,-8, FS5,-8, E5,-4, A4,16, B4,16, D5,16, B4,16,
+
+  E5,-8, E5,-8, D5,-8, CS5,16, B4,-8, A4,16, B4,16, D5,16, B4,16, //18
+  D5,4, E5,8, CS5,-8, B4,16, A4,8, A4,8, A4,8, 
+  E5,4, D5,2, A4,16, B4,16, D5,16, B4,16,
+  FS5,-8, FS5,-8, E5,-4, A4,16, B4,16, D5,16, B4,16,
+  A5,4, CS5,8, D5,-8, CS5,16, B4,8, A4,16, B4,16, D5,16, B4,16,
+
+  D5,4, E5,8, CS5,-8, B4,16, A4,4, A4,8,  //23
+  E5,4, D5,2, REST,4,
+  REST,8, B4,8, D5,8, B4,8, D5,8, E5,4, REST,8,
+  REST,8, CS5,8, B4,8, A4,-4, REST,4,
+  REST,8, B4,8, B4,8, CS5,8, D5,8, B4,8, A4,4,
+  REST,8, A5,8, A5,8, E5,8, FS5,8, E5,8, D5,8,
+  
+  REST,8, A4,8, B4,8, CS5,8, D5,8, B4,8, //29
+  REST,8, CS5,8, B4,8, A4,-4, REST,4,
+  B4,8, B4,8, CS5,8, D5,8, B4,8, A4,4, REST,8,
+  REST,8, E5,8, E5,8, FS5,4, E5,-4, 
+  D5,2, D5,8, E5,8, FS5,8, E5,4, 
+  E5,8, E5,8, FS5,8, E5,8, A4,8, A4,4,
+
+  REST,-4, A4,8, B4,8, CS5,8, D5,8, B4,8, //35
+  REST,8, E5,8, FS5,8, E5,-4, A4,16, B4,16, D5,16, B4,16,
+  FS5,-8, FS5,-8, E5,-4, A4,16, B4,16, D5,16, B4,16,
+  E5,-8, E5,-8, D5,-8, CS5,16, B4,8, A4,16, B4,16, D5,16, B4,16,
+  D5,4, E5,8, CS5,-8, B4,16, A4,4, A4,8, 
+
+   E5,4, D5,2, A4,16, B4,16, D5,16, B4,16, //40
+  FS5,-8, FS5,-8, E5,-4, A4,16, B4,16, D5,16, B4,16,
+  A5,4, CS5,8, D5,-8, CS5,16, B4,8, A4,16, B4,16, D5,16, B4,16,
+  D5,4, E5,8, CS5,-8, B4,16, A4,4, A4,8,  
+  E5,4, D5,2, A4,16, B4,16, D5,16, B4,16,
+   
+  FS5,-8, FS5,-8, E5,-4, A4,16, B4,16, D5,16, B4,16, //45
+  A5,4, CS5,8, D5,-8, CS5,16, B4,8, A4,16, B4,16, D5,16, B4,16,
+  D5,4, E5,8, CS5,-8, B4,16, A4,4, A4,8,  
+  E5,4, D5,2, A4,16, B4,16, D5,16, B4,16,
+  FS5,-8, FS5,-8, E5,-4, A4,16, B4,16, D5,16, B4,16, //45
+  
+  A5,4, CS5,8, D5,-8, CS5,16, B4,8, A4,16, B4,16, D5,16, B4,16,
+  D5,4, E5,8, CS5,-8, B4,16, A4,4, A4,8, 
+
+  E5,4, D5,2, REST,4
+};
+const int neverGonnaGiveYouUpTempo = 150;
+
+const int starWarsMelody[] = {
+  AS4,8, AS4,8, AS4,8,//1
+  F5,2, C6,2,
+  AS5,8, A5,8, G5,8, F6,2, C6,4,  
+  AS5,8, A5,8, G5,8, F6,2, C6,4,  
+  AS5,8, A5,8, AS5,8, G5,2, C5,8, C5,8, C5,8,
+  F5,2, C6,2,
+  AS5,8, A5,8, G5,8, F6,2, C6,4,  
+  
+  AS5,8, A5,8, G5,8, F6,2, C6,4, //8  
+  AS5,8, A5,8, AS5,8, G5,2, C5,-8, C5,16, 
+  D5,-4, D5,8, AS5,8, A5,8, G5,8, F5,8,
+  F5,8, G5,8, A5,8, G5,4, D5,8, E5,4,C5,-8, C5,16,
+  D5,-4, D5,8, AS5,8, A5,8, G5,8, F5,8,
+  
+  C6,-8, G5,16, G5,2, REST,8, C5,8,//13
+  D5,-4, D5,8, AS5,8, A5,8, G5,8, F5,8,
+  F5,8, G5,8, A5,8, G5,4, D5,8, E5,4,C6,-8, C6,16,
+  F6,4, DS6,8, CS6,4, C6,8, AS5,4, GS5,8, G5,4, F5,8,
+  C6,1
+};
+const int starWarsTempo = 150;
+
+const int miiChannelMelody[] = {
+  FS4,8, REST,8, A4,8, CS5,8, REST,8,A4,8, REST,8, FS4,8, //1
+  D4,8, D4,8, D4,8, REST,8, REST,4, REST,8, CS4,8,
+  D4,8, FS4,8, A4,8, CS5,8, REST,8, A4,8, REST,8, F4,8,
+  E5,-4, DS5,8, D5,8, REST,8, REST,4,
+  
+  GS4,8, REST,8, CS5,8, FS4,8, REST,8,CS5,8, REST,8, GS4,8, //5
+  REST,8, CS5,8, G4,8, FS4,8, REST,8, E4,8, REST,8,
+  E4,8, E4,8, E4,8, REST,8, REST,4, E4,8, E4,8,
+  E4,8, REST,8, REST,4, DS4,8, D4,8, 
+
+  CS4,8, REST,8, A4,8, CS5,8, REST,8,A4,8, REST,8, FS4,8, //9
+  D4,8, D4,8, D4,8, REST,8, E5,8, E5,8, E5,8, REST,8,
+  REST,8, FS4,8, A4,8, CS5,8, REST,8, A4,8, REST,8, F4,8,
+  E5,2, D5,8, REST,8, REST,4,
+
+  B4,8, G4,8, D4,8, CS4,4, B4,8, G4,8, CS4,8, //13
+  A4,8, FS4,8, C4,8, B3,4, F4,8, D4,8, B3,8,
+  E4,8, E4,8, E4,8, REST,4, REST,4, AS4,4,
+  CS5,8, D5,8, FS5,8, A5,8, REST,8, REST,4, 
+
+  REST,2, A3,4, AS3,4, //17 
+  A3,-4, A3,8, A3,2,
+  REST,4, A3,8, AS3,8, A3,8, F4,4, C4,8,
+  A3,-4, A3,8, A3,2,
+
+  REST,2, B3,4, C4,4, //21
+  CS4,-4, C4,8, CS4,2,
+  REST,4, CS4,8, C4,8, CS4,8, GS4,4, DS4,8,
+  CS4,-4, DS4,8, B3,1,
+  
+  E4,4, E4,4, E4,4, REST,8,//25
+
+  FS4,8, REST,8, A4,8, CS5,8, REST,8,A4,8, REST,8, FS4,8, //1
+  D4,8, D4,8, D4,8, REST,8, REST,4, REST,8, CS4,8,
+  D4,8, FS4,8, A4,8, CS5,8, REST,8, A4,8, REST,8, F4,8,
+  E5,-4, DS5,8, D5,8, REST,8, REST,4,
+  
+  GS4,8, REST,8, CS5,8, FS4,8, REST,8,CS5,8, REST,8, GS4,8, //5
+  REST,8, CS5,8, G4,8, FS4,8, REST,8, E4,8, REST,8,
+  E4,8, E4,8, E4,8, REST,8, REST,4, E4,8, E4,8,
+  E4,8, REST,8, REST,4, DS4,8, D4,8, 
+
+  CS4,8, REST,8, A4,8, CS5,8, REST,8,A4,8, REST,8, FS4,8, //9
+  D4,8, D4,8, D4,8, REST,8, E5,8, E5,8, E5,8, REST,8,
+  REST,8, FS4,8, A4,8, CS5,8, REST,8, A4,8, REST,8, F4,8,
+  E5,2, D5,8, REST,8, REST,4,
+
+  B4,8, G4,8, D4,8, CS4,4, B4,8, G4,8, CS4,8, //13
+  A4,8, FS4,8, C4,8, B3,4, F4,8, D4,8, B3,8,
+  E4,8, E4,8, E4,8, REST,4, REST,4, AS4,4,
+  CS5,8, D5,8, FS5,8, A5,8, REST,8, REST,4, 
+
+  REST,2, A3,4, AS3,4, //17 
+  A3,-4, A3,8, A3,2,
+  REST,4, A3,8, AS3,8, A3,8, F4,4, C4,8,
+  A3,-4, A3,8, A3,2,
+
+  REST,2, B3,4, C4,4, //21
+  CS4,-4, C4,8, CS4,2,
+  REST,4, CS4,8, C4,8, CS4,8, GS4,4, DS4,8,
+  CS4,-4, DS4,8, B3,1,
+  
+  E4,4, E4,4, E4,4, REST,8,//25
+};
+const int miiChannelTempo = 154;
+
+const int canonInDMelody[] = {
+  FS4,2, E4,2,
+  D4,2, CS4,2,
+  B3,2, A3,2,
+  B3,2, CS4,2,
+  FS4,2, E4,2,
+  D4,2, CS4,2,
+  B3,2, A3,2,
+  B3,2, CS4,2,
+  D4,2, CS4,2,
+  B3,2, A3,2,
+  G3,2, FS3,2,
+  G3,2, A3,2,
+
+  D4,4, FS4,8, G4,8, A4,4, FS4,8, G4,8, 
+  A4,4, B3,8, CS4,8, D4,8, E4,8, FS4,8, G4,8, 
+  FS4,4, D4,8, E4,8, FS4,4, FS3,8, G3,8,
+  A3,8, G3,8, FS3,8, G3,8, A3,2,
+  G3,4, B3,8, A3,8, G3,4, FS3,8, E3,8, 
+  FS3,4, D3,8, E3,8, FS3,8, G3,8, A3,8, B3,8,
+
+  G3,4, B3,8, A3,8, B3,4, CS4,8, D4,8,
+  A3,8, B3,8, CS4,8, D4,8, E4,8, FS4,8, G4,8, A4,2,
+  A4,4, FS4,8, G4,8, A4,4,
+  FS4,8, G4,8, A4,8, A3,8, B3,8, CS4,8,
+  D4,8, E4,8, FS4,8, G4,8, FS4,4, D4,8, E4,8,
+  FS4,8, CS4,8, A3,8, A3,8,
+
+  CS4,4, B3,4, D4,8, CS4,8, B3,4,
+  A3,8, G3,8, A3,4, D3,8, E3,8, FS3,8, G3,8,
+  A3,8, B3,4, G3,4, B3,8, A3,8, B3,4,
+  CS4,8, D4,8, A3,8, B3,8, CS4,8, D4,8, E4,8,
+  FS4,8, G4,8, A4,2,  
+};
+const int canonInDTempo = 140;
+
+const int minuetInGMelody[] = {
+  D5,4, G4,8, A4,8, B4,8, C5,8, //1
+  D5,4, G4,4, G4,4,
+  E5,4, C5,8, D5,8, E5,8, FS5,8,
+  G5,4, G4,4, G4,4,
+  C5,4, D5,8, C5,8, B4,8, A4,8,
+  
+  B4,4, C5,8, B4,8, A4,8, G4,8,//6
+  FS4,4, G4,8, A4,8, B4,8, G4,8,
+  A4,-2,
+  D5,4, G4,8, A4,8, B4,8, C5,8, 
+  D5,4, G4,4, G4,4,
+  E5,4, C5,8, D5,8, E5,8, FS5,8,
+  
+  G5,4, G4,4, G4,4,
+  C5,4, D5,8, C5,8, B4,8, A4,8, //12
+  B4,4, C5,8, B4,8, A4,8, G4,8,
+  A4,4, B4,8, A4,8, G4,8, FS4,8,
+  G4,-2,
+
+  D5,4, G4,8, A4,8, B4,8, C5,8, //1
+  D5,4, G4,4, G4,4,
+  E5,4, C5,8, D5,8, E5,8, FS5,8,
+  G5,4, G4,4, G4,4,
+  C5,4, D5,8, C5,8, B4,8, A4,8,
+  
+  B4,4, C5,8, B4,8, A4,8, G4,8,//6
+  FS4,4, G4,8, A4,8, B4,8, G4,8,
+  A4,-2,
+  D5,4, G4,8, A4,8, B4,8, C5,8, 
+  D5,4, G4,4, G4,4,
+  E5,4, C5,8, D5,8, E5,8, FS5,8,
+  
+  G5,4, G4,4, G4,4,
+  C5,4, D5,8, C5,8, B4,8, A4,8, //12
+  B4,4, C5,8, B4,8, A4,8, G4,8,
+  A4,4, B4,8, A4,8, G4,8, FS4,8,
+  G4,-2,
+
+  B5,4, G5,8, A5,8, B5,8, G5,8,//17
+  A5,4, D5,8, E5,8, FS5,8, D5,8,
+  G5,4, E5,8, FS5,8, G5,8, D5,8,
+  CS5,4, B4,8, CS5,8, A4,4,
+  A4,8, B4,8, CS5,8, D5,8, E5,8, FS5,8,
+
+  G5,4, FS5,4, E5,4, //22
+  FS5,4, A4,4, CS5,4,
+  D5,-2,
+  D5,4, G4,8, FS5,8, G4,4,
+  E5,4,  G4,8, FS4,8, G4,4,
+  D5,4, C5,4, B4,4,
+
+  A4,8, G4,8, FS4,8, G4,8, A4,4, //28
+  D4,8, E4,8, FS4,8, G4,8, A4,8, B4,8,
+  C5,4, B4,4, A4,4,
+  B4,8, D5,8, G4,4, FS4,4,
+  G4,-2,
+};
+const int minuetInDTempo = 180;
+
+const int* melodies[] = {
+  tetrisMelody, pinkPantherMelody, neverGonneGiveYouUpMelody, starWarsMelody, miiChannelMelody, canonInDMelody, minuetInGMelody
+};
+const int tempos[] = {
+  tetrisTempo, pinkPantherTempo, neverGonnaGiveYouUpTempo, starWarsTempo, miiChannelTempo, canonInDTempo, minuetInDTempo
+};
 
 // Misc Vars
+int currentMelodyTempo = 0;
+int musicNotes = sizeof(melodies[currentMelodyTempo]) / sizeof(melodies[currentMelodyTempo][0]) / 2; 
+int musicDivider = 0, noteDuration = 0;
+int wholenote = (60000 * 4) / tempos[currentMelodyTempo];
+int currentNote = 0;
+
 long score = 0;
 long scoreAddition = 1;
 int state = 0;
@@ -136,8 +448,8 @@ void setup() {
     pinMode(latchPins[i], OUTPUT);
     pinMode(clockPins[i], OUTPUT);
   }
-  pinMode(startButton, INPUT);
-  pinMode(interactButton, INPUT);
+  pinMode(startPin, INPUT);
+  pinMode(interactPin, INPUT);
   pinMode(speakerPin, OUTPUT);
 
   renderDifficulty();
@@ -149,23 +461,23 @@ void playTetris() {
     // stop any tones
     noTone(speakerPin);
     // calculates the duration of each note
-    divider = melody[currentNote + 1];
-    if (divider > 0) {
+    musicDivider = melodies[currentMelodyTempo][currentNote + 1];
+    if (musicDivider > 0) {
       // regular note, just proceed
-      noteDuration = (wholenote) / divider;
-    } else if (divider < 0) {
+      noteDuration = (wholenote) / musicDivider;
+    } else if (musicDivider < 0) {
       // dotted notes are represented with negative durations!!
-      noteDuration = (wholenote) / abs(divider);
+      noteDuration = (wholenote) / abs(musicDivider);
       noteDuration *= 1.5; // increases the duration in half for dotted notes
     }
 
     // we only play the note for 90% of the duration, leaving 10% as a pause
-    tone(speakerPin, melody[currentNote], noteDuration * 0.9);
+    tone(speakerPin, melodies[currentMelodyTempo][currentNote], noteDuration * 0.9);
 
     musicPlayLR = currentTime;
     currentNote += 2;
 
-    if (currentNote / 2 >= notes) {
+    if (currentNote / 2 >= musicNotes) {
       currentNote = 0;
     }
   }
@@ -366,8 +678,8 @@ void gameOver() {
 // Main synchronous loop
 void loop() {
   currentTime = millis();
-  startState = digitalRead(startButton);
-  interactState = digitalRead(interactButton);
+  startState = digitalRead(startPin);
+  interactState = digitalRead(interactPin);
 
   playTetris();
 
